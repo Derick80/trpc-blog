f960ff015d9bf2040a34bfd51ac62bb1ba239cbe
Derick80 <derickchoskinson@gmail.com>
2023-05-17 03:25:07 -0500

    DEPS

--- src/components/input-element.tsx
+++ src/components/input-element.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import clsx from "clsx";
 
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
